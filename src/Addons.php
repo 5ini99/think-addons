f12e7cfe020f1ea40489e4f656596a467135658d
xiaobo.sun <xiaobosun@gridinfo.com.cn>
2016-10-08 17:45:13 +0800

    创建初始化完成

--- src/Addons.php
+++ src/Addons.php
@@ -8,62 +8,217 @@
 // +----------------------------------------------------------------------
 // | Author: Byron Sampson <xiaobo.sun@qq.com>
 // +----------------------------------------------------------------------
-namespace think;
+namespace think\addons;
 
-use think\Controller;
+\think\Loader::import('controller/Jump', TRAIT_PATH, EXT);
+
+use think\Config;
+use think\View;
+use think\Db;
 
 /**
- * 插件执行默认控制器
- * Class Addons
- * @package app\common\controller
+ * 插件类
+ * @author Byron Sampson <xiaobo.sun@qq.com>
  */
-class Addons extends Controller
+abstract class Addons
 {
-    // 允许共公访问
-    protected $allow = ['*'];
-
-    // 当前插件操作
-    protected $addon = null;
-    protected $controller = null;
-    protected $action = null;
+    use \traits\controller\Jump;
 
     /**
-     * 插件初始化
+     * 视图实例对象
+     * @var view
+     * @access protected
      */
-    public function _initialize()
-    {
-        $this->addon        = ucfirst($this->request->get('_addon/s', ''));
-        $this->controller   = ucfirst($this->request->get('_controller/s', ''));
-        $this->action       = $this->request->get('_action/s', '');
-        define('ADDONS_URL', __ROOT__ . '/addons/' . $this->addon);
+    protected $view = null;
 
-        $parse_def = config('parse_str');
-        $parse_string = [
-            '__IMG__'       => ADDONS_URL . '/images',
-            '__CSS__'       => ADDONS_URL . '/css',
-            '__JS__'        => ADDONS_URL . '/js',
-        ];
-        if (is_array($parse_def)) {
-            $parse_string = array_merge($parse_def, $parse_string);
+    // 当前错误信息
+    protected $error;
+
+    /**
+     * $info = [
+     *  'name'          => 'Test',
+     *  'title'         => '测试插件',
+     *  'description'   => '用于thinkphp5的插件扩展演示',
+     *  'status'        => 1,
+     *  'author'        => 'byron sampson',
+     *  'version'       => '0.1'
+     * ]
+     */
+    public $info = [];
+    public $addons_path = '';
+    public $config_file = '';
+    public $custom_config = '';
+    public $admin_list = [];
+    public $custom_adminlist = '';
+    public $access_url = [];
+
+    /**
+     * 架构函数
+     * @access public
+     */
+    public function __construct()
+    {
+        // 获取当前插件目录
+        $this->addons_path = ADDON_PATH . $this->getName() . DS;
+        // 读取当前插件配置信息
+        if (is_file($this->addons_path . 'config.php')) {
+            $this->config_file = $this->addons_path . 'config.php';
         }
-        config('parse_str', $parse_string);
+
+        // 初始化视图模型
+        $config = ['view_path' => $this->addons_path];
+        $config = array_merge(Config::get('template'), $config);
+        $this->view = new View($config, Config::get('view_replace_str'));
+
+        // 控制器初始化
+        if (method_exists($this, '_initialize')) {
+            $this->_initialize();
+        }
     }
 
     /**
-     * 插件执行
+     * 获取插件的配置数组
+     * @param string $name 可选模块名
+     * @return array|mixed|null
      */
-    public function execute()
+    final public function getConfig($name = '')
     {
-        if(!empty($this->addon) && !empty($this->controller) && !empty($this->action)){
-            // 获取类的命名空间
-            $class = get_addon_class($this->addon, 'controller') . "\\{$this->controller}";
-            $model = new $class();
-            if ($model === false) {
-                return $this->error(L('addon init fail'));
+        static $_config = array();
+        if (empty($name)) {
+            $name = $this->getName();
+        }
+        if (isset($_config[$name])) {
+            return $_config[$name];
+        }
+        $map['name'] = $name;
+        $map['status'] = 1;
+        $config = Db::name('Addons')->where($map)->value('config');
+        if ($config) {
+            $config = json_decode($config, true);
+        } else {
+            if (is_file($this->config_file)) {
+                $temp_arr = include $this->config_file;
+                foreach ($temp_arr as $key => $value) {
+                    if ($value['type'] == 'group') {
+                        foreach ($value['options'] as $gkey => $gvalue) {
+                            foreach ($gvalue['options'] as $ikey => $ivalue) {
+                                $config[$ikey] = $ivalue['value'];
+                            }
+                        }
+                    } else {
+                        $config[$key] = $temp_arr[$key]['value'];
+                    }
+                }
+                unset($temp_arr);
             }
-            // 调用操作
-            return call_user_func([$model, $this->action]);
         }
-        return $this->error(lang('addon cannot name or action'));
+        $_config[$name] = $config;
+
+        return $config;
     }
+
+    /**
+     * 获取当前模块名
+     * @return string
+     */
+    final public function getName()
+    {
+        $data = explode('\\', get_class($this));
+        return strtolower(array_pop($data));
+    }
+
+    /**
+     * 检查配置信息是否完整
+     * @return bool
+     */
+    final public function checkInfo()
+    {
+        $info_check_keys = ['name', 'title', 'description', 'status', 'author', 'version'];
+        foreach ($info_check_keys as $value) {
+            if (!array_key_exists($value, $this->info)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * 加载模板和页面输出 可以返回输出内容
+     * @access public
+     * @param string $template 模板文件名或者内容
+     * @param array $vars 模板输出变量
+     * @param array $replace 替换内容
+     * @param array $config 模板参数
+     * @return mixed
+     * @throws \Exception
+     */
+    public function fetch($template = '', $vars = [], $replace = [], $config = [])
+    {
+        if (!is_file($template)) {
+            $template = '/' . $template;
+        }
+        // 关闭模板布局
+        $this->view->engine->layout(false);
+
+        echo $this->view->fetch($template, $vars, $replace, $config);
+    }
+
+    /**
+     * 渲染内容输出
+     * @access public
+     * @param string $content 内容
+     * @param array $vars 模板输出变量
+     * @param array $replace 替换内容
+     * @param array $config 模板参数
+     * @return mixed
+     */
+    public function display($content, $vars = [], $replace = [], $config = [])
+    {
+        // 关闭模板布局
+        $this->view->engine->layout(false);
+
+        echo $this->view->display($content, $vars, $replace, $config);
+    }
+
+    /**
+     * 渲染内容输出
+     * @access public
+     * @param string $content 内容
+     * @param array $vars 模板输出变量
+     * @return mixed
+     */
+    public function show($content, $vars = [])
+    {
+        // 关闭模板布局
+        $this->view->engine->layout(false);
+
+        echo $this->view->fetch($content, $vars, [], [], true);
+    }
+
+    /**
+     * 模板变量赋值
+     * @access protected
+     * @param mixed $name 要显示的模板变量
+     * @param mixed $value 变量的值
+     * @return void
+     */
+    public function assign($name, $value = '')
+    {
+        $this->view->assign($name, $value);
+    }
+
+    /**
+     * 获取当前错误信息
+     * @return mixed
+     */
+    public function getError()
+    {
+        return $this->error;
+    }
+
+    //必须实现安装
+    abstract public function install();
+
+    //必须卸载插件方法
+    abstract public function uninstall();
 }
--- src/helper.php
+++ src/helper.php
@@ -1,1 +1,126 @@
 <?php
+// +----------------------------------------------------------------------
+// | thinkphp5 Addons [ WE CAN DO IT JUST THINK IT ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2016 http://www.zzstudio.net All rights reserved.
+// +----------------------------------------------------------------------
+// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+// +----------------------------------------------------------------------
+// | Author: Byron Sampson <xiaobo.sun@qq.com>
+// +----------------------------------------------------------------------
+
+use think\Hook;
+use think\Config;
+
+// 插件目录
+define('ADDON_PATH', ROOT_PATH . 'addons' . DS);
+
+// 定义路由
+\think\Route::get('addons/execute', "\\think\\addons\\AddonsController@execute");
+
+// 如果插件目录不存在则创建
+if (!is_dir(ADDON_PATH)) {
+    mkdir(ADDON_PATH, 0777, true);
+}
+
+// 注册类的根命名空间
+\think\Loader::addNamespace('addons', ADDON_PATH);
+
+// 闭包初始化行为
+Hook::add('action_begin', function () {
+    // 获取系统配置
+    $data = \think\Config::get('app_debug') ? [] : cache('hooks1');
+    if (empty($data)) {
+        $addons = (array)Config::get('addons');
+        foreach ($addons as $key => $values) {
+            if (is_string($values)) {
+                $values = explode(',', $values);
+            } else {
+                $values = (array)$values;
+            }
+            $addons[$key] = array_map('get_addon_class', $values);
+            \think\Hook::add($key, $addons[$key]);
+        }
+        cache('hooks', $addons);
+    } else {
+        Hook::import($data, false);
+    }
+});
+
+
+/**
+ * 处理插件钩子
+ * @param string $hook 钩子名称
+ * @param mixed $params 传入参数
+ * @return void
+ */
+function hook($hook, $params = [])
+{
+    \think\Hook::listen($hook, $params);
+}
+
+/**
+ * 获取插件类的类名
+ * @param $name 插件名
+ * @param string $type 返回命名空间类型
+ * @return string
+ */
+function get_addon_class($name, $type = 'hook')
+{
+    switch ($type) {
+        case 'controller':
+            $namespace = "\\addons\\" . strtolower($name) . "\\controller";
+            break;
+        default:
+            $namespace = "\\addons\\" . strtolower($name) . "\\" . ucfirst(strtolower($name));
+    }
+
+    return $namespace;
+}
+
+/**
+ * 获取插件类的配置文件数组
+ * @param string $name 插件名
+ * @return array
+ */
+function get_addon_config($name)
+{
+    $class = get_addon_class($name);
+    if (class_exists($class)) {
+        $addon = new $class();
+        return $addon->getConfig();
+    } else {
+        return [];
+    }
+}
+
+/**
+ * 插件显示内容里生成访问插件的url
+ * @param $url
+ * @param array $param
+ * @return bool|string
+ */
+function addon_url($url, $param = [])
+{
+    $url = parse_url($url);
+    $case = config('URL_CASE_INSENSITIVE');
+    $addons = $case ? parse_name($url['scheme']) : $url['scheme'];
+    $controller = $case ? parse_name($url['host']) : $url['host'];
+    $action = trim($case ? strtolower($url['path']) : $url['path'], '/');
+
+    /* 解析URL带的参数 */
+    if (isset($url['query'])) {
+        parse_str($url['query'], $query);
+        $param = array_merge($query, $param);
+    }
+
+    /* 基础参数 */
+    $params = array(
+        '_addon' => $addons,
+        '_controller' => $controller,
+        '_action' => $action,
+    );
+    $params = array_merge($params, $param); //添加额外参数
+
+    return url('@addons/execute', $params);
+}
